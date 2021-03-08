2291ae416833fdcd53d83935b70f1c524cdac78e
Sam-app <samuel.meb@icloud.com>
2021-03-08 09:34:09 +0000

    installed pod and fixed some eslint problems

--- App.js
+++ App.js
@@ -6,31 +6,24 @@
  * @flow strict-local
  */
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  StatusBar,
-  View
-} from 'react-native';
+import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
 
-import { PcodeEntries } from './src/components/PcodeEntries.component'
+import { PcodeEntries } from './src/components/PcodeEntries.component';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.screen}>
-        
-         <PcodeEntries />
-  
+        <PcodeEntries />
       </SafeAreaView>
     </>
   );
 };
 const styles = StyleSheet.create({
-  screen : {
+  screen: {
     flex: 1,
-  }
+  },
 });
 
 export default App;
